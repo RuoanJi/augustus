4a48de1276c76a4075163b2385b2b4464f84cf7d
ruoan_ji <jiruoan@gmail.com>
2022-09-10 21:29:53 -0700

    Fix image display

--- src/building/image.c
+++ src/building/image.c
@@ -831,11 +831,11 @@ int building_image_get(building *b)
         case BUILDING_DEPOT:
             switch (scenario_property_climate()) {
             case CLIMATE_NORTHERN:
-                return assets_get_image_id("Econ_Logistics", "Cart Depot N ON");
+                return assets_get_image_id("Logistics", "Cart Depot N ON");
             case CLIMATE_DESERT:
-                return assets_get_image_id("Econ_Logistics", "Cart Depot S ON");
+                return assets_get_image_id("Logistics", "Cart Depot S ON");
             default:
-                return assets_get_image_id("Econ_Logistics", "Cart Depot C ON");
+                return assets_get_image_id("Logistics", "Cart Depot C ON");
             }
         default:
             return 0;
